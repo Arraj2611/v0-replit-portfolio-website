f541cd1e8de30e1e421a868bee1c42d6caf09aa8
Arraj2611 <arrajeevaken2611@gmail.com>
2025-05-19 22:30:41 +0530

    minor fixes

--- components/animations/particle-background.tsx
+++ components/animations/particle-background.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useRef, useState } from "react"
-import { useTheme } from "@/components/theme-provider"
+import { useTheme } from "next-themes"
 
 interface Particle {
   x: number
